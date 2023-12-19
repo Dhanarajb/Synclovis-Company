9b42a9146209190998f5f58e6431094fc9a5aea2
Dhanarajb <dhanarajbhaskar690@gmail.com>
2023-12-19 17:08:52 +0530

    updated

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Synclovis company
 
-//RestApi
-//Virtual Dom
-//useEffectt
-//null and undefined
-//var and let/
-//when we use arrow function
-//inharitance
-//why main in java
-//advantage of frameworks
+//RestApi - answered
+//Virtual Dom -answered
+//useEffectt - answered
+//null and undefined - answered
+//var and let/ - answered
+//when we use arrow function - answered
+//inharitance - answered
+//why main in java - answered
+//advantage of frameworks - answered
+//Singleton pattern - not answered
 
-// input = 'DDRRRRPPKLL';
+// input = 'DDRRRRPPKLL'; - answered
 // Output = "D2R4P2K1L2"
+
 
 // initi count =1
 //initi resultstring =' '
@@ -21,24 +23,24 @@
 //resultstring+= i+count> 1
 // count=1
 
-function occuranceFunc(input) {
-  if (!input) {
-    return ''; // Return an empty string for an empty input
-  }
-  let resultstring = '';
-  let count = 1;
+// function occuranceFunc(input) {
+//   if (!input) {
+//     return ''; // Return an empty string for an empty input
+//   }
+//   let resultstring = '';
+//   let count = 1;
 
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] === input[i + 1]) {
-      count++;
-    } else {
-      resultstring += input[i] + (count > 1 ? count : '');
-      count = 1;
-    }
-  }
-  return resultstring;
-}
-input = 'DDRRRRPPKLL';
-const result = occuranceFunc(input);
-console.log(result);
-console.log('hello');
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i] === input[i + 1]) {
+//       count++;
+//     } else {
+//       resultstring += input[i] + (count > 1 ? count : '');
+//       count = 1;
+//     }
+//   }
+//   return resultstring;
+// }
+// input = 'DDRRRRPPKLL';
+// const result = occuranceFunc(input);
+// console.log(result);
+// console.log('hello');
